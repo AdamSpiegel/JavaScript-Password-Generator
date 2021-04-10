9b2d9aba30e2a59404aef494d0777e67a459e514
Adam Spiegel <adamspiegel23@gmail.com>
2021-04-10 11:17:58 -0400

    final update

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# JavaScript-Password-Generator
+# **JavaScript-Password-Generator**
+
+## Project Description & Goals
+   The project goals for this assignment were to develop a functional Javascript Password Generator, based off of starter code that was provided to us.  Using JavaScript alone, our goals were to have the user generate a randomized password based off of specific criteria.  This included a password character length of   8-128 characters.  
+   
+   Additionally, the user should have the capability to have the option of selecting from upper case letters, lower case letters, numeric values and special characters (via pop up confirm/prompt windows). 
+
+   The overall goal of this project was to have a functional password generator that provides the strongest password possible for the security of the user.
+
+
+## Action Items 
+
+    1  Initially, all variables and arrays had to be written and determined for proper functionality.
+
+    2  Next, a generate password function had to be created in order to house all the components to fuel the password generator. 
+
+    3  An initial prompt was created, asking the users how many characters they want to be displayed.  This was followed by a seperate (if) conditional to not let the user continue if the value is not between 8-128 characters. 
+
+    4  Next, an (else) conditional was put into place, to confirm with the user which particular set of array they wanted included in their password (lowercase, uppercase, numberical values and special characters) 
+
+    5 Following this step, 4 seperate (if) conditionals were created to pull from each of the variable options listed for the user to pick from for their password.
+
+    6 Finally, a function was created at the close of the JS to pull the password options back into the actual generator to be displayed for the user.
+
+
+
+ <img src="Screenshot (17).png" alt="JS Password Generator Screenshot">
+
+
+
+## Live Link to Deployed Page
+
+ https://adamspiegel.github.io/JavaScript-Password-Generator/
+
+ 
+
+# Contact Information
+* Email - AdamSpiegel23@gmail.com
+* GitHub - https://github.com/AdamSpiegel
+* LinkedIn - www.linkedin.com/in/adam-spiegel-3086687
+
--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 // All password variables for generator
 
-// All variables to store password criteria - 4.9.21 AS
-var pwlowerCase = "abcdefghijklmnopqrstuvwxyz";
-var pwupperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var pwspecialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-var pwNumbers = "0123456789";
-var pwLength = 0;
-var finalPassword = "" 
-
-// var confirmLower;
-// var confirmUpper;
-// var confirmNumber;
-// var confirmSpecial;
-// var userChoice;
+// All variables to store password criteria - 4.8.21 AS
+var pwlowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var pwupperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var pwspecialCharacters = ['#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','['];
+var pwNumbers = [0,1,2,3,4,5,6,7,8,9];
+var characterPool = [];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -32,31 +25,54 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword (){
 
-  // Beginning the process of generating password, by asking user for character length of 8-126, and cancelling further actions if outside parameters. - 4.9.21 AMS
+  // Beginning the process of generating password, by asking user for character length of 8-126, and cancelling further actions if outside parameters. - 4.9.21 AS
 
-    pwLength = prompt ("How many characters would you like your password to contain? Choose 8-128")
-    console.log (pwLength)
+    var numberofCharacters = prompt ("How many characters would you like your password to contain? Choose 8-128")
+    console.log (numberofCharacters)
 
-    // Confirms character length is not below 8 or above 128. Stops proceeding forward otherwise. 4.9.21 AMS
-    if (pwLength <8 || pwLength > 128){
-     pwLength = alert("You must choose between 8 and 128"); 
+    // Confirms character length is not below 8 or above 128. Stops proceeding forward otherwise. 4.9.21 AS
+    if (numberofCharacters <8 || numberofCharacters > 128){
+      alert ("You must choose between 8 and 128"); 
+      return;
 
      }
 
-    // Begin confirmation process of included character options in password - 4.9.21 AMS
-    else{
-    pwlowerCase = confirm ("Would you like your password to contain lowercase letters?");
-    pwupperCase = confirm ("Would you like your password to contain uppercase letters?");
-    pwspecialCharacters = confirm ("Would you like your password to contain special characters?");
-    pwNumbers = confirm ("Would you like your password to contain numbers?");
-    }
+    // Begin confirmation window process of included character options in password - 4.9.21 AS
+    
 
-    var pwLength = 8-128,
-    pwLength =  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+,-./:;<=>?@[]^_`{|}~",
-    retVal = "";
-    for (var i = 0, n = pwLength.length; i < length; ++i) {
-    retVal += pwSet.charAt(Math.floor(Math.random() * n));
-    }
-    return retVal;
+    if (confirm ("Would you like your password to contain lowercase letters?")) {
+       characterPool = characterPool.concat(pwlowerCase)
+     }
 
-  }
+     if (confirm ("Would you like your password to contain uppercase letters?")) {
+       characterPool = characterPool.concat(pwupperCase)
+     }
+
+     if (confirm ("Would you like your password to contain special characters?")) {
+       characterPool = characterPool.concat(pwspecialCharacters)
+     }
+
+     if (confirm ("Would you like your password to contain numbers?")) {
+       characterPool = characterPool.concat(pwNumbers)
+     }
+
+     console.log (characterPool)
+
+      var result = [];
+
+      for ( var i = 0; i < numberofCharacters; i++ ) {
+      result.push(characterPool[Math.floor(Math.random() * 
+      characterPool.length)]);
+     }
+
+      console.log (result)
+
+      return result.join('');
+     }
+
+
+
+   
+
+
+
